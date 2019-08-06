3d1f5099ccc59346581cb51d3da9345ba597ee4f
Jonas Funcke <jonas@funcke.work>
2019-08-06 21:25:49 +0200

    fixing login route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,11 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
+    if !user.nil? && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: "logged in!"
     else
-      redirect_to 'new'
+      redirect_to new_session_path
     end
   end
 
